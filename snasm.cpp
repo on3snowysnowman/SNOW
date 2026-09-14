efc0f4c390a772e263b13ce327e8706fedf27003
Joel Height <on3snowysnowman@gmail.com>
2026-09-13 20:27:38 -0400

    Modified ISA and began rewriting assembler

--- snasm.cpp
+++ snasm.cpp
@@ -1,13 +1,17 @@
 // snasm.cpp
 
+
 #include <iostream>
-#include <cstdint>
+#include <string>
 #include <fstream>
-#include <cerrno>
-#include <cstring>
-#include <unordered_map>
+#include <cstdint>
 
 
+// Types -----------------------------------------------------------------------
+
+using reg_T = uint8_t;
+
+// Represents a sectoin inside the assembly file.
 enum class SECTION
 {
     TEXT,
@@ -15,8 +19,8 @@ enum class SECTION
     UNRESOLVED
 };
 
-
-enum class OPERATION
+// Types instructions can represent
+enum class INSTR_TYPE
 {
     ADD,
     ADDI,
@@ -31,6 +35,18 @@ enum class OPERATION
     DIV,
     BCOND,
     BLINK,
+    AND,
+    NAND,
+    OR,
+    NOR,
+    XOR,
+    NXOR,
+    NOT,
+    SEXTB,
+    LSH,
+    RSH,
+    NOOP,
+    HALT,
     MOV,
     CMP,
     SUBI,
@@ -38,375 +54,235 @@ enum class OPERATION
     RET
 };
 
-// Global Variables ------------------------------------------------------------
-static const char *parsed_file = nullptr;
-
-static std::ifstream in_file;
-static std::ofstream out_file;
-
-static std::unordered_map<std::string_view, uint16_t> local_labels;
-
-// Section currently being parsed.
-static SECTION current_section = SECTION::UNRESOLVED;
-
-// Current parsed line number in the source file.
-static size_t current_line = 0;
-
-// Current byte offset within the .text section.
-static size_t current_text_offset = 0;
-
-// Current byte offset within the .data section.
-static size_t current_data_offset = 0;
-
-
-
-
-// Containers ------------------------------------------------------------------
-
-// Represents a label that has been defined by the program and is visible within
-// this unit.
-struct DefinedLabel
+struct AddInstr
 {
-    // Location where the symbol is defined. This will not be unresolved, as it
-    // is a local label.
-    SECTION section;
+    reg_T dest;
+    reg_T sr1;
+    reg_T sr2;
+};
 
-    // Offset from the section where this symbol is defined. 
-    size_t offset;
+struct AddImInstr
+{
+    reg_T dest;
+    int8_t immediate;
+};
+
+struct MovLInstr
+{
+    reg_T dest;
+    int8_t immediate;
+};
+
+struct MovHInstr
+{
+    reg_T dest;
+    int8_t immediate;
+};
+
+struct SubInstr
+{
+    reg_T dest;
+    reg_T sr1;
+    reg_T sr2;
+};
+
+struct LodBInstr
+{
+    reg_T dest;
+    reg_T addr;
+    int8_t offset;
+};
+
+struct LodWInstr
+{
+    reg_T dest;
+    reg_T addr;
+    int8_t offset;
+};
+
+struct StrBInstr
+{
+    reg_T dest;
+    reg_T addr;
+    int8_t offset;
+};
+
+struct StrWInstr
+{
+    reg_T dest;
+    reg_T addr;
+    int8_t offset;
+};
+
+struct MulInstr
+{
+    reg_T dest;
+    reg_T src1;
+    reg_T src2;
+};
+
+struct DivInstr
+{
+    reg_T dest;
+    reg_T src1;
+    reg_T src2;
+};
+
+enum class BCondType
+{
+    EQ,
+    NE,
+    GT,
+    LT
+};
+
+struct BCondInst
+{
+    BCondType cond;
+    int16_t offset; // Only 10 bits are available.
+};
+
+struct BUncondInst
+{
+    int16_t offset; // Only 12 bits are available.
+};
+
+struct AndInstr
+{
+    reg_T dest;
+    reg_T src;
+};
+
+struct NandInstr
+{
+    reg_T dest;
+    reg_T src;
+};
+
+struct OrInstr
+{
+    reg_T dest;
+    reg_T src;
+};
+
+struct NorInstr
+{
+    reg_T dest;
+    reg_T src;
+};
+
+struct XorInstr
+{
+    reg_T dest;
+    reg_T src;
+};
+
+struct XnorInstr
+{
+    reg_T dest;
+    reg_T src;
+};
+
+struct NotInstr
+{
+    reg_T dest;
+    reg_T src;
+};
+
+struct SextBInstr
+{
+    reg_T dest;
+    reg_T src;
+};
+
+struct LshInstr
+{
+    reg_T dest;
+    int8_t immediate;
+};
+
+struct RshInstr
+{
+    reg_T dest;
+    int8_t immediate;
+};
+
+struct NoopInstr {};
+
+struct HaltInstr {};
+
+struct BLinkInstr
+{
+    reg_T link;
+    reg_T addr;
+};
+
+struct CmpInstr
+{
+    reg_T src1;
+    reg_T src2;
+};
+
+struct MovInstr
+{
+    reg_T dest;
+    reg_T src;
+};
+
+struct SubImInstr
+{
+    reg_T dest;
+    int8_t immediate;
+};
+
+struct CallInstr
+{
+    int16_t offset; // Offset in instruction count, not bytes.
+};
+
+struct RetInstr {};
+
+
+struct Instruction
+{
+    // Type this instruction represents.
+    INSTR_TYPE type;    
+
+    union 
+    {
+        
+    } as;
+    
 };
 
 
 
-// Represents an entry in the relocation table.
-// struct RelocTableEntry
-// {
 
-// }
+// Global Variables ------------------------------------------------------------
 
+static std::ifstream in_file;
 
-// Represents a label that 
-// struct GlobalLabel
-// {
-//     std::string name;
+// Path to the file being parsed
+static const char *parsed_file;
 
-//     // Location where the symbol is defined.
-//     SECTION section; 
-    
-//     // Offset from section where this symbol is defined. 0 if the section is
-//     // UNRESOLVED.
-//     size_t offset;
-// };
+// Current parsed line inside the file being parsed.
+static size_t current_line;
 
 
-static void print_err_location(size_t col = 0)
-{
-    std::cout << parsed_file << ':' << current_line << ':' << col + 1;
-}
 
-static void skip_whitespace(const std::string &str, size_t &idx)
-{
-    while(idx < str.size() && str[idx] == ' ') { ++idx; }
-}
-
-static void print_unexp_eol(size_t col = 0)
-{
-    print_err_location(col);
-    std::cout << " -> Unexpected end of line.\n";
-}
-
-static void consume_reg(const std::string &str, size_t &idx, bool expect_comma)
-{
-    skip_whitespace(str, idx);
-
-    std::string_view view(str.data() + idx, str.size() - idx);
-
-    size_t end_reg_ident = view.find_first_of(" ,\t\n");
-
-    view = view.substr(0, end_reg_ident);
-
-    if(view.size() != 2 || 
-        view.at(0) != 'x' ||
-        !std::isdigit(view.at(1)))
-    {
-        print_err_location(idx);
-        std::cout << " -> Expected register, got: \"" << view << "\"\n";
-        exit(-1);
-    }
-
-    // Consume register
-    idx += 2;
-
-    if(expect_comma)
-    {
-        if(idx >= str.size())
-        {
-            print_unexp_eol(str.size());
-            exit(-1);
-        }
-
-        if(str[idx] != ',')
-        {
-            print_err_location(idx);
-            std::cout << " -> Expected comma.\n";
-            exit(-1);
-        }
-        
-        // Consume comma
-        ++idx;
-    }
-}
-
-static void enforce_ins_section()
-{
-    if(current_section == SECTION::DATA)
-    {
-        print_err_location();
-        std::cout << " -> Instructions cannot be defined in the data "
-            "section.\n";
-        exit(-1);
-    }
-}
-
-/**
- * @brief Given a position inside a string, attempts to parse an 
- * operation, crashing if invalid. Index is passed in by reference and is 
- * modified directly. 
- * 
- * @param str String to parse.
- * @param idx Index to begin parsing.
- * 
- * @return OPERATION Parsed operation. 
- */
-static OPERATION get_op(const std::string &str, size_t &idx)
-{
-    // Skip any whitespace before the operation
-    skip_whitespace(str, idx);
-
-    std::string_view view(str.data() + idx, str.size() - idx);
-
-    size_t end_op_ident = view.find_first_of(" \t\n");
-
-    view = view.substr(0, end_op_ident);
-
-    // Consume operator ident.
-    idx += end_op_ident;
-
-    if(view == "add")
-    {
-        enforce_ins_section();
-
-        consume_reg(str, idx, true);
-        consume_reg(str, idx, true);
-        consume_reg(str, idx, false);
-    }
-    else if(view == "addi")
-    {
-        enforce_ins_section();
-
-        
-
-    }
-    else if(view == "movl")
-    {
-
-    }
-    else if(view == "movh")
-    {
-
-    }
-    else if(view == "sub")
-    {
-
-    }
-    else if(view == "lodb")
-    {
-
-    }
-    else if(view == "lodw")
-    {
-
-    }
-    else if(view == "strb")
-    {
-
-    }
-    else if(view == "strw")
-    {
-
-    }
-    else if(view == "mul")
-    {
-
-    }
-    else if(view == "div")
-    {
-
-    }
-    else if(view == "beq" || view == "bne" || view == "blt" || view == "bgt")
-    {
-
-    }
-    else if(view == "bl")
-    {
-
-    }
-    else if(view == "db")
-    {
-
-    }
-    else if(view == "dw")
-    {
-
-    }
-    else
-    {
-        print_err_location();
-        std::cout << " -> Unkown operation: \"" << view << "\"\n";
-        exit(-1);
-    }
-
-
-    return OPERATION::ADD;
-}
-
-
-
-/**
- * @brief Given an index inside a string, parses till the end of the string 
- * validating operations and checking if the given operation 
- * updates the offset in the given section.
- * 
- * @param str String to parse.
- * @param idx Index to start parsing from.
- */
-static void check_op_update_offset(const std::string &str, size_t idx)
-{
-    if(idx >= str.size()) { return; }
-
-    get_op(str, idx);
-
-    // while(idx < str.size())
-    // {
-
-
-
-
-
-    // }
-}
-
-
-static void parse_labels()
-{
-    std::string line;
-    current_line = 0;
-    
-    while(std::getline(in_file, line))
-    {
-        size_t line_idx = 0;
-        ++current_line;
-        if(line.size() == 0) { continue; }
-
-        // Section
-        if(line.at(0) == '.')
-        {
-            std::string_view view{line};
-
-            size_t end = view.find_first_of(" \n");
-
-            view = view.substr(0, end);
-
-            if(view == ".text")
-            {
-                print_err_location();
-                std::cout << " -> Parsed section: TEXT\n";
-                current_section = SECTION::TEXT;
-            }
-            else if(view == ".data")
-            {
-                print_err_location();
-                std::cout << " -> Parsed section: DATA\n";
-                current_section = SECTION::DATA;
-            }
-            else
-            {
-                print_err_location();
-                std::cout << " -> Invalid section identifier.\n";
-                exit(-1);
-            }
-
-            line_idx += end;
-        }
-
-        // Label
-        else if(line.at(0) != ' ')
-        {
-            std::string_view view{line};
-
-            size_t end = view.find_first_of(":");
-
-            if(end == std::string_view::npos ||    
-                view.find_first_of(" ") <= end)
-            {
-                print_err_location();
-                std::cout << " -> Invalid label.\n";
-                exit(-1);
-            }
-
-            view = view.substr(0, end);
-
-            print_err_location();
-            std::cout << " -> Parsed label: " << view << '\n';
-        
-            // +1 to consume the ':'
-            line_idx += end + 1;
-        }
-
-        // No more text in the line
-        if(line_idx >= line.size()) { continue; }
-
-        check_op_update_offset(line, line_idx);
-    }
-}
-
-static void assemble()
-{
-    parse_labels();
-}
 
 
 int main(int argc, char **argv)
 {
     if(argc != 3)
     {
-        std::cout << "Usage: snasm <input_file> <output_file>\n";
+        std::cout << "Usage: snasm <in_file> <out_file>\n";
         return -1;
     }
 
-    const char *in_file_path = argv[1];
-    const char *out_file_path = argv[2];
 
-    parsed_file = in_file_path;
 
-    in_file.open(in_file_path);
-
-    if(!in_file.is_open()) 
-    {
-        std::cerr << "Failed to open file: "
-                  << std::strerror(errno) << '\n';
-        return -1;
-    }
-
-    out_file.open(out_file_path);
-
-    if(!out_file.is_open())
-    {
-        std::cerr << "Failed to open file: "
-                  << std::strerror(errno) << '\n';
-        return -1;
-    }
-
-    assemble();
+    std::cout << "Hello World!\n";
 
     return 0;
 }
